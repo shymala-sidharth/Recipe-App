dc08be88c898c2770dd2c06fda41155a6715fcf9
Shymala Sidharth <shymala.sidharth22@gmail.com>
2023-02-16 11:24:43 +1300

    added some form inputs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Recipe-App</title>
+      <link rel="icon" href="/favicon.ico" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700;800&display=swap"
+					rel="stylesheet"
+				/>
+
+      </Head>
+  
+  <Component {...pageProps} />
+
+  </>
+
+  )
 }
 
 export default MyApp
