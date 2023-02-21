53808d5bf790b0da17974f7a671af91cd7e8b877
Shymala Sidharth <shymala.sidharth22@gmail.com>
2023-02-21 16:42:11 +1300

    updated styling now working on functionality 3

---
+++ pages/api/search.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+
+  const { NEXT_PUBLIC_RAPIDAPI_KEY } = process.env;
+
+  const options = {
+    method: 'GET',
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch',
+    params: {
+      query: req.query.keyword,
+      diet: req.query.diet,
+      excludeIngredients: req.query.exclude,
+      number: '20',
+      offset: '0',
+    },
+    headers: {
+      'x-rapidapi-host':
+				'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+			'x-rapidapi-key': NEXT_PUBLIC_RAPIDAPI_KEY
+    }
+  }
+
+  try {
+    let response = await axios.request(options);
+    res.status(200).json(response.data);
+  } catch (error: any) {
+    console.error(error.response);
+  }
+
+}
