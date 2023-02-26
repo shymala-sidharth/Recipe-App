360ac45ab119d3545df03cc2a665d6ce8edee0f7
Shymala Sidharth <shymala.sidharth22@gmail.com>
2023-02-26 14:18:00 +1300

    connecting API

--- pages/api/search.tsx
+++ pages/api/search.tsx
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
     res.status(200).json(response.data);
   } catch (error: any) {
     console.error(error.response);
-    console.log(error.response.data);
+    console.log(error.response);
   }
 
 }
